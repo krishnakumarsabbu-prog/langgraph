20c7b2ab4ffe637875c7c4702f7d05a66e3005cf
KrishnaKumarSabbu <krishnakumarsabbu@gmail.com>
2026-03-24 21:10:58 +0530

    Updated ParallelNode.tsx

--- src/components/LangGraph/ParallelNode.tsx
+++ src/components/LangGraph/ParallelNode.tsx
@@ -50,6 +50,13 @@ export const ParallelNode: React.FC<ParallelNodeProps> = ({ id, data, selected }
         className="w-3 h-3 bg-gray-400"
         style={{ top: '70%' }}
       />
+      <Handle
+        type="source"
+        position={Position.Right}
+        id="right32"
+        className="w-3 h-3 bg-gray-400"
+        style={{ top: '70%' }}
+      />
     </div>
   );
 };
